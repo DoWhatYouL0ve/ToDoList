d6e9368a7b6abf1dede445b028fa7d913dde10af
DoWhatYouL0ve <uladzimirpryshchapionak@protonmail.com>
2021-03-04 02:03:56 +0100

    Updates

---
+++ precache-manifest.770b5d6c2f71e6b9db4cdb79562251a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5f09110af4509b9da801ad957f6fe0",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "a0fe893345b20c301c04",
+    "url": "/ToDoList/static/css/main.41747de1.chunk.css"
+  },
+  {
+    "revision": "4e0044aabe0d0c4638a3",
+    "url": "/ToDoList/static/js/2.f19a5572.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList/static/js/2.f19a5572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fe893345b20c301c04",
+    "url": "/ToDoList/static/js/main.32ba4bc1.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
